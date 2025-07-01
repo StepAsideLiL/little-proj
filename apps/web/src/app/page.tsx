f171216d05ea635a5ba2a9d4696c44d8b9cc1041
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-01 21:17:38 +0600

    feat: update next.config.js and enhance page layout with instructions chore: remove unused components and exports from design-system

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,40 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <main className="mx-auto w-full max-w-5xl py-10">
-      <h1 className="text-center text-2xl">Welcome to Little Proj👋</h1>
+    <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-1 py-10">
+      <h1 className="text-2xl font-bold">Hi! 👋 </h1>
+      <p>Welcome to Little Proj.</p>
+
+      <div className="h-10"></div>
+
+      <div>
+        <h3>Edit The following files to get started:</h3>
+        <ul className="list-inside list-disc">
+          <li>
+            <code className="font-mono text-sm">package.json</code>{" "}
+            <span>[name field]</span>
+          </li>
+          <li>
+            <code className="font-mono text-sm">
+              apps/web/src/lib/site-config/index.ts
+            </code>{" "}
+            <span>[name, description, author name and url field]</span>
+          </li>
+          <li>
+            <code className="font-mono text-sm">apps/web/public</code>{" "}
+            <span>
+              [generate android-chrome-192x192.png, android-chrome-512x512.png,
+              apple-touch-icon.png, favicon.ico, favicon-16x16.png,
+              favicon-32x32.png, site.webmanifest from{" "}
+              <Link href={"https://favicon.io/"} className="underline">
+                favicon.io
+              </Link>{" "}
+              and paste in public folder]
+            </span>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
--- packages/design-system/src/icons/index.tsx
+++ packages/design-system/src/icons/index.tsx
@@ -1,15 +1,7 @@
+import * as L from "lucide-react";
 import * as R from "@remixicon/react";
 
-export default {
-  AlignCenter: R.RiAlignCenter,
-  AlignLeft: R.RiAlignLeft,
-  AlignJustify: R.RiAlignJustify,
-  AlignRight: R.RiAlignRight,
-  Bold: R.RiBold,
-  Delete: R.RiDeleteBin2Line,
-  Italic: R.RiItalic,
-  Heading: R.RiHeading,
-  Github: R.RiGithubFill,
-  Paragraph: R.RiParagraph,
-  Underline: R.RiUnderline,
-} as const;
+const Icons = { LucideIcon: L, RemixIcons: R };
+
+export type Icons = typeof Icons;
+export default Icons;
